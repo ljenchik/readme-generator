245821afd4a2e577b16aa57360526bc4e23675c7
Olena Pelagenko <84686704+ljenchik@users.noreply.github.com>
2023-02-25 21:52:46 +0000

    Added all questions and table odf content

--- index.js
+++ index.js
@@ -27,14 +27,20 @@ const promptUser = () => {
       name: "installation",
     },
     {
+      type: "input",
+      message: "Add installation contributors, links to tutorials and materials you used in your project",
+      name: "contribution",
+    },
+    {
         type: "input",
         message: "Add usage",
         name: "usage",
       },
       {
         type: "list",
-        message: "Choose license",
+        message: "Choose a license",
         name: "license",
+        choices: ["1", "2", "3"]
       },
       {
         type: "input",
@@ -51,14 +57,49 @@ const generateHTML = (answers) =>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-  <title>Document</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+  
+  
+  <link rel="stylesheet" href="css/style.css">
+  <title>Readme-generator</title>
 </head>
 <body>
-  <div class="jumbotron jumbotron-fluid">
   <div class="container">
-    <h1 class="display-4">Username is ${answers.githubName}</h1>
+    <div><i class="fa-regular fa-list"></i> Readme.md</div>
+    <hr/>
+    <h1 class="display-4" id="title">${answers.title}</h1>
+    <hr />
+    <h2 id="description">Description</h2>
+    <div>${answers.description}</div>
+    <hr />
+    <h2>Table of Content</h2>
+      <ul>
+        <li><a href="#title">Title</a></li>
+        <li><a href="#description">Description</a></li>
+        <li><a href="#installation">Installation</a></li>
+        <li><a href="#usage">Usage</a></li>
+        <li><a href="#contribution">Contribution</a></li>
+        <li><a href="#license">License</a></li>
+        <li><a href="#tests">Tests</a></li>
+      </ul>
+    <hr />
+    <h2 id="installation">Installation</h2>
+    <div>${answers.installation}</div>
+    <hr />
+    <h2 id="usage">Usage</h2>
+    <div>${answers.usage}</div>
+    <hr />
+    <h2 id="contribution">Contribution</h2>
+    <div>${answers.contribution}</div>
+    <hr />
+    <h2 id="license">License</h2>
+    <div>${answers.license}</div>
+    <hr />
+    <h2 id="tests">Tests</h2>
+    <div>${answers.tests}</div>
+    <hr />
+    
   </div>
-</div>
 </body>
 </html>`;
 
